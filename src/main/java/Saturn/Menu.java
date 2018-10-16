522b2274a122459415f83c53cd58e9f8a4e06ce3
ArrantRook <kumar.vivaan.in@gmail.com>
2018-10-16 19:10:18 +0530

    Commiting the source added menu class

---
+++ src/main/java/Saturn/Menu.java
@@ -0,0 +1,13 @@
+package Saturn;
+
+import java.util.Scanner;
+public class Menu {
+    public static void display()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Welcome to Carzzy");
+        System.out.println("Select from one of the following:");
+        System.out.println("1. Sign Up \t 2. Log In \t 3. Search Cars \t 4. Check Our Prices");
+        String choice = sc.nextLine();
+    }
+}
